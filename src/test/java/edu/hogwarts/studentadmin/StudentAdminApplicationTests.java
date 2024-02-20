45ad0936201dd6980e6e9a367e06d079261f1e22
emilvn <emilvnielsen@hotmail.com>
2024-02-20 13:50:30 +0100

    test

--- src/test/java/edu/hogwarts/studentadmin/StudentAdminApplicationTests.java
+++ src/test/java/edu/hogwarts/studentadmin/StudentAdminApplicationTests.java
@@ -3,11 +3,14 @@ package edu.hogwarts.studentadmin;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
 @SpringBootTest
 class StudentAdminApplicationTests {
 
     @Test
     void contextLoads() {
+        assertTrue(true);
     }
 
 }
